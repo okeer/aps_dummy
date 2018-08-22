df0e10d627e11b9b82d715d5504116327522596e
Sergey Orlov <okeer.md@gmail.com>
2018-08-22 11:07:15 +0700

    init

---
+++ scripts/service-template.php
@@ -0,0 +1,32 @@
+<?php
+
+	define('APS_DEVELOPMENT_MODE', true);
+	require "aps/2/runtime.php";	
+
+	/**
+	* @type("http://aps-standard.org/samples/vpsdemo/server-template/1.0")
+	* @implements("http://aps-standard.org/types/core/resource/1.0")
+	*/
+
+	class service extends \APS\ResourceBase
+	{
+
+		public function provision(){
+
+		}
+
+		public function retrieve(){
+
+		}
+
+		public function upgrade(){
+
+		}
+
+		public function unprovision(){
+
+		}
+
+		
+	}
+?>
---
+++ ui/view-template.js
@@ -0,0 +1,21 @@
+define([
+		"dojo/_base/declare",
+		"aps/View"
+		
+	],
+	function (
+		declare,
+		View
+		
+	) {
+	return declare(View, {
+		init: function() {
+			/* Declare the data sources */
+	
+			/* Define and return widgets */
+			return [
+		        
+		];}
+		
+});});
+
