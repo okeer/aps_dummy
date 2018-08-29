1376c39e5d46522c20e3288c48310231ed5fc388
Sergey Orlov <okeer.md@gmail.com>
2018-08-29 15:43:41 +0700

    Add Home tile

---
+++ ui/plugins/dummyDashboardPligin.js
@@ -0,0 +1,9 @@
+define([
+    "dojo/_base/declare",
+    "aps/nav/ViewPlugin"
+], function (declare, ViewPlugin) {
+    return declare(ViewPlugin, {
+      /* Assign view-plugin properties here */
+      apsType: "http://sorlov.tld/aps/test/tenant"
+    });
+});
